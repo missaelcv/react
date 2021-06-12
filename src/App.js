c6b70077f6c810b8884d61caee7c9914c07d46d2
missaelcv <missaelcv0220@gmail.com>
2021-06-11 21:07:15 -0400

    creando componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React from 'react';
 import './App.css';
+
+function Helloworld(){
+  return (
+  <div>Helloworld</div>
+  )
+}
+
 
 function App() {
   return (
-    <div>Hello Missael
-    </div>
+    <div>this is my componert is: <Helloworld/></div>
   );
 }
 
