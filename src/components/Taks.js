70c14e06438dd20c1e27f643530c7eb01a4d0bf3
missaelcv <missaelcv0220@gmail.com>
2021-06-13 19:48:40 -0400

    Aplicacion Creada que Didive los datos en multiples Componentes

---
+++ src/components/Taks.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Task extends Component {
+    render () {
+        const {task} = this.props;
+
+        return  <div> 
+        {task.title} - 
+        {task.description} - 
+        {task.done} - 
+        {task.id}
+        <input type="checkbox"/>
+        <button>X</button>
+        </div>
+    }
+}
+
+export default Task;
