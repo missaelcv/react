bd1dde3a0ec734b67f35c59b388759d265fd9cc3
missaelcv <missaelcv0220@gmail.com>
2021-06-13 18:40:45 -0400

    Crear carpeta componente e exportarlo

---
+++ src/components/tasks.js
@@ -0,0 +1,10 @@
+import { extGlobChars } from "picomatch/lib/constants";
+import React, { Component, Components} from "react";
+
+class Tasks  extends Component() {
+    render() {
+        return <h1>Tasks</h1>
+    }
+}
+
+export default tasks;
