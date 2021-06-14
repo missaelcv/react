9db3282d1ecae1a8f07f7af9a52e88a2ff4bc61b
missaelcv <missaelcv0220@gmail.com>
2021-06-13 20:52:03 -0400

    Agregando Estilos CSS a y funciones con retorno

--- src/components/Taks.js
+++ src/components/Taks.js
@@ -1,18 +1,37 @@
+
 import React, { Component } from 'react'
 
 class Task extends Component {
+
+    StyleComplete (){
+         return {
+             fontSize: '20px',
+             color: this.props.task.done ? 'gray' : 'black',
+             textDecoration: this.props.task.done ? 'line-through' : 'none'
+         }
+    }
+
     render () {
         const {task} = this.props;
-
-        return  <div> 
+       
+        return  <p style={this.StyleComplete()}> 
         {task.title} - 
         {task.description} - 
         {task.done} - 
         {task.id}
         <input type="checkbox"/>
-        <button>X</button>
-        </div>
+        <button style={btnDelete}>X</button>
+        </p>
     }
 }
 
+const btnDelete= {
+    fontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    borde: 'none',
+    padding: '10px 15px',
+    borderRadius: '50%',
+    cursor: 'pointer' 
+}
 export default Task;
