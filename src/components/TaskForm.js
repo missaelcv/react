3722616078e4a2d4834a2c8e96f5c3a025ff75c2
missaelcv <missaelcv0220@gmail.com>
2021-06-14 21:44:51 -0400

    Creando Componente TaskForm

---
+++ src/components/TaskForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class TaskForm extends Component {
+    render() {
+        return (
+            <form>
+                <input type="text" placeholder="Write a Task"/>
+            </form>
+        )
+    }
+}
+export default TaskForm;
