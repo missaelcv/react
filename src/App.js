00366c11507a2f80a09a55dc3a3c84927d53e25d
missaelcv <missaelcv0220@gmail.com>
2021-06-14 21:33:24 -0400

    Importe el TaskForm

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import tasks from './sample/tasks.json';
 import Tasks from './components/Tasks';
+import TaskForm from './components/TaskForm';
 
 
 class App extends Component{
@@ -11,6 +12,7 @@ class App extends Component{
 
 render() { 
   return <div>
+    <TaskForm/>
     <Tasks tasks= {this.state.tasks}/>
   </div>
   }
