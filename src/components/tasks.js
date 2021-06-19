67a1ecb5b1d1be17215433d277f8cb45832a2097
missaelcv <missaelcv0220@gmail.com>
2021-06-18 23:22:15 -0400

    {this.deleteTask} en componente Tasks.js

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -5,7 +5,12 @@ import Task from './Taks';
 
 class Tasks extends Component {
     render () {
-        return this.props.tasks.map(task => <Task task={task} key={task.id}/>)
+        return this.props.tasks.map(task => 
+        <Task 
+        task={task} 
+        key={task.id}
+        deleteTask={this.deleteTask} 
+        />)
     }
 }
 
