ec742f5925d295977c55aa512c4058768ff23ab6
missaelcv <missaelcv0220@gmail.com>
2021-06-20 19:25:59 -0400

    async componentDidMount con el state

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 export default class Posts extends Component {
+
+        state = {
+            posts: []
+        }
+
+  async componentDidMount(){
+      const res =  await fetch('https://jsonplaceholder.typicode.com/posts')
+       const data = await res.json();
+       this.setState({posts: data})
+    }
     render() {
         return (
             <div>
-                
+                <h1>Publicaciones Posts</h1>
             </div>
         )
     }
